70220afc1a3316716814a391473eec0c517440b3
Jonathan Caiza <dele.yfdeveloper@gmail.com>
2026-03-10 16:21:26 -0500

    feat(core): add global http error interceptor

--- src/app/core/interceptors/http-error-interceptor.ts
+++ src/app/core/interceptors/http-error-interceptor.ts
@@ -1,5 +1,26 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
 
 export const httpErrorInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      let errorMessage = 'Ocurrió un error desconocido';
+
+      if (error.error instanceof ErrorEvent) {
+        errorMessage = `Error de cliente: ${error.error.message}`;
+      } else {
+        if (error.status === 400) {
+          errorMessage = error.error.message || 'Datos inválidos enviados al servidor.';
+        } else if (error.status === 404) {
+          errorMessage = error.error.message || 'Recurso no encontrado.';
+        } else {
+          errorMessage = `Código de error ${error.status}: ${error.message}`;
+        }
+      }
+
+      console.error('El interceptor capturó el error:', errorMessage);
+
+      return throwError(() => new Error(errorMessage));
+    }),
+  );
 };
